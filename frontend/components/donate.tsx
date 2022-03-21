e3fe9a6674cdc40b7c619851c39d47c4700f482d
Yuzuki Aida <dev@nzws.me>
2022-03-21 19:04:33 +0900

    update

--- frontend/components/donate.tsx
+++ frontend/components/donate.tsx
@@ -5,12 +5,25 @@ const Donate = styled.div`
   padding: 25px 50px;
 `
 
+const Mirror = styled.div`
+  font-weight: bold;
+`
+
 export default function DonateBox(): JSX.Element {
   return (
     <Donate>
-      Love Shields? Please consider{' '}
-      <a href="https://opencollective.com/shields">donating</a> to sustain our
-      activities
+      <Mirror>
+        This is a mirror site of{' '}
+        <a href="https://shields.io" rel="noopener noreferrer" target="_blank">
+          Shields.io
+        </a>{' '}
+        for @dotplants.
+      </Mirror>
+      <div>
+        Love Shields? Please consider{' '}
+        <a href="https://opencollective.com/shields">donating</a> to sustain our
+        activities
+      </div>
     </Donate>
   )
 }
